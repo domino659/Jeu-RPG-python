0398e6adec5180210bf417015bd9845ea9d47dec
Martin SION <martin@sionfamily.com>
2021-05-11 11:10:56 +0200

    Update assets/map

--- Assets/Backgrounds/TMX/Plant.tsx
+++ Assets/Backgrounds/TMX/Plant.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.6.0" name="Plant" tilewidth="32" tileheight="32" tilecount="256" columns="16">
- <image source="TX Plant.png" width="512" height="512"/>
+ <image source="../IMG/TX Plant.png" width="512" height="512"/>
 </tileset>
--- Assets/Backgrounds/TMX/Struct.tsx
+++ Assets/Backgrounds/TMX/Struct.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.6.0" name="Struct" tilewidth="32" tileheight="32" tilecount="256" columns="16">
- <image source="TX Struct.png" width="512" height="512"/>
+ <image source="../IMG/TX Struct.png" width="512" height="512"/>
 </tileset>
